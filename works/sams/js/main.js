b5d73a8a9271dc7041b89186b081cf31ae5d67ce
Steve Ling <steveling@gmail.com>
2017-06-13 11:00:09 -0700

    adding initial template for Sam's Club styleguide

--- works/sams/js/main.js
+++ works/sams/js/main.js
@@ -22,6 +22,18 @@ $(document).on('click', function(e) {
   });
 });
 
+// add to cart //
+
+$(document).on('click', function(e) {
+  $('[data-toggle="collapse"]').each(function() {
+    //the 'is' for buttons that trigger popups
+    //the 'has' for icons within a button that triggers a popup
+    if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {
+      $(this).popover('hide').data('bs.popover').inState.click = false // fix for BS 3.3.6
+    }
+  });
+});
+
 // animate floating label //
 
 $(".form-group input").change(function() {
