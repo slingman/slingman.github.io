067a7064595463908cb38cada4819b0a081deb94
Steve Ling <steveling@gmail.com>
2018-06-28 16:08:15 -0700

    added show-hide password functionality on login password form

--- works/sams/js/main.js
+++ works/sams/js/main.js
@@ -94,3 +94,39 @@ $(document).ready(function(){
   });
 
 });
+
+
+
+$(".toggle-password").click(function() {
+
+  $(this).toggleClass("fa-eye fa-eye-slash");
+  var input = $($(this).attr("toggle"));
+  if (input.attr("type") == "password") {
+    input.attr("type", "text");
+  } else {
+    input.attr("type", "password");
+  }
+});
+
+
+$(function(){
+  $('.hide-show').show();
+  $('.hide-show span').addClass('show')
+
+  $('.hide-show span').click(function(){
+    if( $(this).hasClass('show') ) {
+      $(this).text('Hide');
+      $('input[name="login[password]"]').attr('type','text');
+      $(this).removeClass('show');
+    } else {
+       $(this).text('Show');
+       $('input[name="login[password]"]').attr('type','password');
+       $(this).addClass('show');
+    }
+  });
+
+	$('form button[type="submit"]').on('click', function(){
+		$('.hide-show span').text('Show').addClass('show');
+		$('.hide-show').parent().find('input[name="login[password]"]').attr('type','password');
+	});
+});
